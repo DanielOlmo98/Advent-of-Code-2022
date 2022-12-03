e4e6591749708f46c7e884157ed3fe4d5403607d
DanielOlmo98 <danielolmo0798@gmail.com>
2022-12-03 15:22:12 +0100

    added sum

--- d1/day1.hs
+++ d1/day1.hs
@@ -6,7 +6,14 @@ quickSort [] = []
 quickSort (list_1st:list_tail) = quickSort left_side ++ [list_1st] ++ quickSort right_side
                              where
                                 left_side  = [a | a <- list_tail, a <= list_1st]
-                                right_side = [b | b <- list_tail, b > list_1st]
+                                right_side = [b | b <- list_tail, b >  list_1st]
+
+reverseQuickSort [] = []
+reverseQuickSort (list_1st:list_tail) = reverseQuickSort left_side ++ [list_1st] ++ reverseQuickSort right_side
+                             where
+                                left_side  = [a | a <- list_tail, a >= list_1st]
+                                right_side = [b | b <- list_tail, b <  list_1st]
+
 f :: [String] -> [Int]
 f = map read
 
@@ -22,9 +29,16 @@ getInvs path = do
   pure $ sumSortInvs (groupElfInvs input)
 
 sumSortInvs :: [[Int]] -> [Int]
-sumSortInvs invs = quickSort (map sum invs)
+sumSortInvs invs = reverseQuickSort (map sum invs)
+
+top3Invs :: [Int] -> Int
+top3Invs invs = sum $ take 3 invs
 
 main :: IO ()
 main = do
   invs <- getInvs "input.txt"
-  print invs
+  print "Top 1"
+  print $ take 1 invs
+  print "Sum top 3"
+  print $ top3Invs invs
+  -- print invs
