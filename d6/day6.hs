9c8ac56aefeb93ea9fa7e8a42b94e39e9ded6975
Daniel <danielolmo0798@gmail.com>
2022-12-06 19:57:36 +0100

    haskell day 6 solution

---
+++ d6/day6.hs
@@ -0,0 +1,22 @@
+import Data.List (findIndex, nub, tails)
+
+windows :: Int -> [a] -> [[a]]
+windows n xs = map (take n) (tails xs)
+
+isUnique :: Eq a => [a] -> Bool
+isUnique win = length win == length (nub win)
+
+getMarkerStart :: Int -> [Char] -> Int
+getMarkerStart n stream = length $ takeWhile (not . isUnique) (windows n stream)
+
+getPacketStart :: Int -> FilePath -> IO Int
+getPacketStart n path = do
+  input <- readFile path
+  pure $ n + getMarkerStart n input
+
+main :: IO ()
+main = do
+  answer <- getPacketStart 4 "input.txt"
+  print answer
+  answer <- getPacketStart 14 "input.txt"
+  print answer
